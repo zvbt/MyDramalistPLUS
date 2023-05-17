c2b435b3e90d7d32f457e1752df90be8a18900b0
zvbt <zvbt@pm.me>
2023-05-17 22:35:06 +0200

    compressed ads.js

--- ads.js
+++ ads.js
@@ -1,9 +1,1 @@
-var ads=[
-    "*://ap.lijit.com/www/sovrn_beacon_standalone/sovrn_standalone_beacon.js?iid=10909913/*",
-    "*://securepubads.g.doubleclick.net/tag/js/gpt.js/*",
-    "*://secure.quantserve.com/quant.js/*",
-    "*://c.amazon-adsystem.com/aax2/apstag.js/*",
-    "*://www.google-analytics.com/analytics.js/*",
-    "*://kiwi.mdldb.net/*",
-    "*://d3lcz8vpax4lo2.cloudfront.net/*",
-]
+var ads=["*://ap.lijit.com/www/sovrn_beacon_standalone/sovrn_standalone_beacon.js?iid=10909913/*","*://securepubads.g.doubleclick.net/tag/js/gpt.js/*","*://secure.quantserve.com/quant.js/*","*://c.amazon-adsystem.com/aax2/apstag.js/*","*://www.google-analytics.com/analytics.js/*","*://kiwi.mdldb.net/*","*://d3lcz8vpax4lo2.cloudfront.net/*"];
