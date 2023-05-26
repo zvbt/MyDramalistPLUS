d932d54ef7cac00faf588a15a53ac9d41ef76434
Cyadine <61088560+zvbt@users.noreply.github.com>
2023-05-26 10:23:17 +0200

    fixed css error

--- content.js
+++ content.js
@@ -5,12 +5,31 @@ document.addEventListener("readystatechange", (event) => {
   if (event.target.readyState === "complete") {
     try {
       let location = window.location.href;
+
+      //mdl+ tag
       if (location.match("https://mydramalist.com/profile/Cyadine")) {
         let cyadine = document.querySelector(
-          "#content > div > div.container-fluid.profile-container > div > div.col-lg-8.col-md-8 > div.box > div.box-header.box-navbar > div.profile-header.mdl-component > div.hidden-sm-down > h1"
+          "#content > div > div.container-fluid.profile-container > div > div.col-lg-8.col-md-8 > div.box > div.box-header.box-navbar > div.profile-header.mdl-component > div.row.no-gutter > div.col-xs-8.col-sm-8 > h1 > span"
         );
 
-        cyadine.innerHTML = `<span style="font-size: 18px;cursor: pointer;background-color: #6b03fc;color: hsla(0,0%,100%,0.87);padding: 2px 4px;text-align: center;border-radius: 5px;" onClick='window.open("https://github.com/zvbt/MyDramalistPLUS", "_blank");'>MDL+</span> Cyadine`;
+        cyadine.innerHTML = `<span style="font-size: 18px;cursor: pointer;background-color: #6b03fc;color: hsla(0,0%,100%,0.87);padding: 2px 4px;text-align: center;border-radius: 5px;" onClick='window.open("https://github.com/zvbt/MyDramalistPLUS", "_blank");'>MDL+</span>`;
+      }
+
+      //remove hidden-md-up
+      if (location.match("https://mydramalist.com/profile/")) {
+        let username = document.querySelector(
+          "#content > div > div.container-fluid.profile-container > div > div.col-lg-8.col-md-8 > div.box > div.box-header.box-navbar > div.profile-header.mdl-component > div.row.no-gutter.hidden-md-up"
+        );
+
+        username.classList.add("md-up");
+
+        username.classList.remove("hidden-md-up");
+
+        if (username.classList.contains("md-up")) {
+          console.log("class already fixed");
+        }
+
+        username.classList.toggle("md-up");
       }
 
       let userProfile = document.querySelector(
@@ -49,7 +68,24 @@ document.addEventListener("readystatechange", (event) => {
   }
 });
 
-let langAlert="The extension MyDramaListPLUS is still in beta and work only with the english version of MyDramaList.\nAfter closing this dialog you will be redirected to\nhttps://mydramalist/com";function checklang(){window.location.href.match("https://fr.mydramalist.com")&&(alert(langAlert),window.location.href="https://mydramalist.com/"),window.location.href.match("https://es.mydramalist.com")&&(alert(langAlert),window.location.href="https://mydramalist.com/"),window.location.href.match("https://it.mydramalist.com")&&(alert(langAlert),window.location.href="https://mydramalist.com/"),window.location.href.match("https://nl.mydramalist.com")&&(alert(langAlert),window.location.href="https://mydramalist.com/"),window.location.href.match("https://pt.mydramalist.com")&&(alert(langAlert),window.location.href="https://mydramalist.com/"),window.location.href.match("https://br.mydramalist.com")&&(alert(langAlert),window.location.href="https://mydramalist.com/"),window.location.href.match("https://ro.mydramalist.com")&&(alert(langAlert),window.location.href="https://mydramalist.com/")}
+let langAlert =
+  "The extension MyDramaListPLUS is still in beta and work only with the english version of MyDramaList.\nAfter closing this dialog you will be redirected to\nhttps://mydramalist/com";
+function checklang() {
+  window.location.href.match("https://fr.mydramalist.com") &&
+    (alert(langAlert), (window.location.href = "https://mydramalist.com/")),
+    window.location.href.match("https://es.mydramalist.com") &&
+      (alert(langAlert), (window.location.href = "https://mydramalist.com/")),
+    window.location.href.match("https://it.mydramalist.com") &&
+      (alert(langAlert), (window.location.href = "https://mydramalist.com/")),
+    window.location.href.match("https://nl.mydramalist.com") &&
+      (alert(langAlert), (window.location.href = "https://mydramalist.com/")),
+    window.location.href.match("https://pt.mydramalist.com") &&
+      (alert(langAlert), (window.location.href = "https://mydramalist.com/")),
+    window.location.href.match("https://br.mydramalist.com") &&
+      (alert(langAlert), (window.location.href = "https://mydramalist.com/")),
+    window.location.href.match("https://ro.mydramalist.com") &&
+      (alert(langAlert), (window.location.href = "https://mydramalist.com/"));
+}
 
 document.addEventListener("readystatechange", (event) => {
   if (event.target.readyState === "complete") {
